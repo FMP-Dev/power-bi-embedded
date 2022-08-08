f417012bf81741ae4e6cf7a01b9fdc28746c1054
FMP-Dev <109966286+FMP-Dev@users.noreply.github.com>
2022-08-08 17:36:28 +0300

    Upd

--- src/Services/Report.php
+++ src/Services/Report.php
@@ -4,17 +4,15 @@
 namespace PowerBiEmbedded\Services;
 
 
-use PowerBiEmbedded\Responses\DeleteGroupResponse;
-use PowerBiEmbedded\Responses\GetGroupResponse;
-use PowerBiEmbedded\Responses\PostGroupResponse;
+use PowerBiEmbedded\Responses\GetReportInGroupResponse;
 
 
 /**
- * Class Dataset.
+ * Class Report.
  *
  * @package PowerBiEmbedded\Services
  */
-class Group extends Service
+class Report extends Service
 {
 
 	/**
@@ -22,54 +20,18 @@ class Group extends Service
 	 *
 	 * @var string
 	 */
-	protected $base_url = 'https://api.powerbi.com/v1.0/myorg/groups/';
-
-
-	/**
-	 * Create a new group.
-	 *
-	 * @see https://docs.microsoft.com/en-gb/rest/api/power-bi/groups/creategroup
-	 *
-	 * @param string $name
-	 * @param bool $use_workspace_v2
-	 * @return PostGroupResponse
-	 * @throws \GuzzleHttp\Exception\GuzzleException
-	 */
-	public function postGroup(string $name, bool $use_workspace_v2)
-	{
-		$endpoint = $this->base_url . ($use_workspace_v2 ? '?workspaceV2=true' : '');
-
-		return new PostGroupResponse($this->client->request('POST', $endpoint, ['name' => $name]));
-	}
-
+	protected $base_url = 'https://api.powerbi.com/v1.0/myorg/';
 
 	/**
-	 * Delete group by id.
+	 * Returns the specified report from the specified workspace.
+     *
+	 * @see https://docs.microsoft.com/ru-ru/rest/api/power-bi/reports/get-report-in-group
 	 *
-	 * @see https://docs.microsoft.com/en-gb/rest/api/power-bi/groups/deletegroup
-	 *
-	 * @param string $id
-	 * @return DeleteGroupResponse
+	 * @return GetReportInGroupResponse
 	 * @throws \GuzzleHttp\Exception\GuzzleException
 	 */
-	public function deleteGroupById(string $id)
-	{
-		$endpoint = $this->base_url . $id;
-
-		return new DeleteGroupResponse($this->client->request('DELETE', $endpoint));
-	}
-
-
-	/**
-	 * Get group list.
-	 *
-	 * @see https://docs.microsoft.com/en-gb/rest/api/power-bi/groups/getgroups
-	 *
-	 * @return GetGroupResponse
-	 * @throws \GuzzleHttp\Exception\GuzzleException
-	 */
-	public function getGroups()
-	{
-		return new GetGroupResponse($this->client->request('GET', $this->base_url));
+	public function getGroups($workspace_id, $report_id)	{
+        $endpoint = $this->base_url. 'groups/' . $workspace_id . '/reports/' . $report_id;
+		return new GetReportInGroupResponse($this->client->request('GET', $endpoint));
 	}
 }
